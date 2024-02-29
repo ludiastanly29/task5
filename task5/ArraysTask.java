86858e53beaa4d744dad55e8e6726fb9ba27b584
Ludia <160195657+ludiastanly29@users.noreply.github.com>
2024-02-29 12:36:01 +0530

    Update ArraysTask.java

--- task5/ArraysTask.java
+++ task5/ArraysTask.java
@@ -1,26 +1,31 @@
 package task5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ArraysTask {
 
 	public static void main(String[] args) {
-		/*
-		 * Create a Scanner object to take user input
-		 */
+	/*
+	 * Create a Scanner object to take user input
+	 */
         Scanner scanner = new Scanner(System.in);
 
         /*
          * Prompt the user for input
          */
-        System.out.println("Enter strings (type 'exit' to stop):");
+       
+        System.out.println("Enter the total number of strings to store: ");
+        int totalCount = scanner.nextInt();
+		scanner.nextLine();  // Consume newline character
         /*
          * Collect user input into a list
          */
-        List<String> inputList = collectUserInput(scanner);
+	System.out.println("Enter strings");
+        List<String> inputList = collectUserInput(scanner,totalCount);
         
         /*
          * 
@@ -34,10 +39,20 @@ public class ArraysTask {
 	/*
 	 * Collect user input into a list
 	 */
-	private static List<String> collectUserInput(Scanner scanner) {
-        List<String> inputList = Stream.generate(scanner::nextLine).takeWhile(line -> !line.equalsIgnoreCase("exit"))
-                .collect(Collectors.toList());
-
+	private static List<String> collectUserInput(Scanner scanner, int totalCount) {
+	List<String> inputList = new ArrayList<>();
+	String[] inputArray = new String[totalCount];
+	int i=0;
+	String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (i >= totalCount) {
+                break;
+            }
+            inputArray[i] = input;
+            i++;
+        }
+        inputList =Arrays.asList(inputArray);
         return inputList;
     }
 }
